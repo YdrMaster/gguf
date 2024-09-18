3817b2b96c590294d0e1250bc7ca280b2adee3eb
pwhMass <a203865235@gmail.com>
2024-09-18 16:44:06 +0800

    feat: 添加Q2K反量化

--- ggml-quants/src/structs/q2_k.rs
+++ ggml-quants/src/structs/q2_k.rs
@@ -1,5 +1,6 @@
 ﻿use super::{DeltaMin, _256};
 use crate::{DataBlock, Quantize};
+use itertools::Itertools;
 
 #[repr(C)]
 pub struct Q2K {
@@ -24,6 +25,30 @@ impl Quantize<f32, _256> for Q2K {
         todo!()
     }
     fn dequantize(&self) -> [f32; _256] {
-        todo!()
+        let mut ans = [0.; _256];
+        let (delta, min) = self.delta_min.to_f32();
+        let de_qs = self
+            .qs
+            .iter()
+            .flat_map(|&q| [q & 0b11, q >> 2 & 0b11, q >> 4 & 0b11, q >> 6 & 0b11]);
+        let dl_and_ml = self
+            .scales
+            .iter()
+            .map(|sc| (delta * ((sc & 0xF) as f32), min * ((sc >> 4) as f32)));
+
+        let de_qs_chunks = de_qs.chunks(16);
+        let result = de_qs_chunks
+            .into_iter()
+            .zip(dl_and_ml)
+            .map(|(qs, (delta_l, min_l))| qs.map(move |n| delta_l * n as f32 - min_l))
+            .flatten();
+
+        let mut count = 0;
+        for (i, q) in result.enumerate() {
+            ans[i] = q;
+            count += 1;
+        }
+        assert!(count == _256);
+        ans
     }
 }
