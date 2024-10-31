4784c7c30e5fa49414179781e6c0399a929cc719
YdrMaster <ydrml@hotmail.com>
2024-10-31 17:33:48 +0800

    fix: 添加测试，改正 q4_0 反量化
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- ggml-quants/src/structs/q4_0.rs
+++ ggml-quants/src/structs/q4_0.rs
@@ -31,8 +31,8 @@ impl Quantize<f32, _32> for Q4_0 {
         }
 
         let delta = max / -8.;
-        let recip = if max == 0. { 0. } else { delta.recip() };
-        let f = |x| ((x * recip + 8.5) as u8).min(15);
+        let recip = delta.recip();
+        let f = |x: f32| (x * recip + 8.5).min(15.) as u8;
 
         let (l, h) = data.split_at(_32 / 2);
         Self {
@@ -43,7 +43,7 @@ impl Quantize<f32, _32> for Q4_0 {
 
     fn dequantize(&self) -> [f32; _32] {
         let delta = self.delta.to_f32();
-        let f = |x| (x - 8) as f32 * delta;
+        let f = |x| (x as i32 - 8) as f32 * delta;
 
         let mut ans = [0.; _32];
         let (l, h) = ans.split_at_mut(_32 / 2);
@@ -53,4 +53,29 @@ impl Quantize<f32, _32> for Q4_0 {
         }
         ans
     }
+}
+
+#[test]
+fn test_q4_0() {
+    use crate::test_utils::{Diff, ErrorCollector};
+    use rand::Rng;
+    use std::iter::zip;
+
+    let mut data = [0.0f32; _32];
+    rand::thread_rng().fill(&mut data);
+
+    let q4_0 = Q4_0::quantize(&data);
+    let deq = <Q4_0 as Quantize<f32, _32>>::dequantize(&q4_0);
+
+    let mut ec = ErrorCollector::new(8e-2, 0.);
+    for (a, b) in zip(data, deq) {
+        ec.push(Diff::new(a, b))
+    }
+    println!("{ec}");
+
+    for &i in ec.outliers() {
+        println!("{} vs {}", data[i], deq[i]);
+    }
+
+    assert!(ec.outliers().is_empty());
 }
